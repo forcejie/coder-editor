58b43f100c324d7bb5de4b69bf5dd1991f05e845
forcejie <2963301696@qq.com>
2023-04-06 21:43:53 +0800

    wangeditor mononaeditor

---
+++ vite-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <JsonEditor v-model="code"></JsonEditor>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import JsonEditor from "./components/JsonEditor.vue";
+
+const code = ref("");
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.container {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+}
+</style>
